e54a94dc8c48667edfb879c20ce7d605d57a71ef
Alexandra Muñoz <m.guioth@utp.edu.co>
2026-02-21 17:00:37 -0500

    Add comments explaining the legal scenario

--- E2.pl
+++ E2.pl
@@ -1,3 +1,8 @@
+%Enunciado: La ley dice que es un crimen para un Estadounidense vender armas a naciones
+%hostiles. Corea del Sur, enemigo de Estados Unidos, tiene algunos misiles, y todos
+%sus misiles les fueron vendidos por el Coronel West, quien es Estadounidense.
+%Pruebe que el Col. West es un criminal.
+
 %Hechos
 nacion_hostil(corea_sur).
 estadounidense(west).
